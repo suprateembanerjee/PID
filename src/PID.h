7800e77901faf69bd666bd6fffedb8629027ab45
Suprateem Banerjee <dolphinextreme48@gmail.com>
2021-02-04 13:13:44 +0530

    Initial commit

---
+++ src/PID.h
@@ -0,0 +1,60 @@
+#ifndef PID_H
+#define PID_H
+
+class PID {
+ public:
+  /**
+   * Constructors
+   */
+  PID();
+  PID(double Kp_, double Ki_, double Kd_);
+
+  /**
+   * Destructor.
+   */
+  virtual ~PID();
+
+  /**
+   * Initialize PID.
+   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
+   */
+  void set_coeffs(double Kp_, double Ki_, double Kd_);
+
+  /**
+   * Update the PID error variables given cross track error.
+   * @param cte The current cross track error
+   */
+  void UpdateError(double cte);
+
+  /**
+   * Calculate the total PID error.
+   * @output The total PID error
+   */
+  double TotalError();
+
+  /**
+   * Estimate optimal parameters using Twiddle.
+   * @output The range of parameter values.
+   */
+  double Twiddle(double tol);
+
+ private:
+  /**
+   * PID Errors
+   */
+  double p_error;
+  double i_error;
+  double d_error;
+
+  /**
+   * PID Coefficients
+   */ 
+  double Kp;
+  double Ki;
+  double Kd;
+
+  double prev_cte;
+  double total_cte;
+};
+
+#endif  // PID_H
